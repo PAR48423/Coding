e44afe9c1f04413480821e27dd2a1a0a692bf0c3
jdbc <jdbc@gmail.com>
2021-10-10 09:12:15 +0530

    Initial commit

---
+++ src/Collections/Arraylist.java
@@ -0,0 +1,8 @@
+package Collections;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        System.out.println("from main");
+        System.out.println("HIi how are you");
+    }
+}
